dc020a82d32cd569c29a5f1958053265cfad4b39
Salvador Cabral <salvadormhoc@gmail.com>
2023-02-21 16:31:47 +0000

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new # Needed to instantiate the form_with
@@ -18,19 +18,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path(@tasks)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     # No need for app/views/tasks/update.html.erb
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     # No need for app/views/tasks/destroy.html.erb
     redirect_to tasks_path, status: :see_other
@@ -40,5 +36,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
